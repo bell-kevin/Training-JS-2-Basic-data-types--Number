e2ef4f69d816e8232e763ba23cd261f35ebae141
Kevin Bell <bellKevin@pm.me>
2022-06-29 00:45:32 +0000

    Create simpleMathjs.js

---
+++ simpleMathjs.js
@@ -0,0 +1,27 @@
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+  var a=v1   
+  var b=v1   
+  return a+b;
+}
+//Please refer to the example above to complete the following functions
+function equal2(){
+  var a= 150  //set number value to a
+  var b= 50  //set number value to b
+  return a-b;
+}
+function equal3(){
+  var a=   10//set number value to a
+  var b=   10//set number value to b
+  return a*b;
+}
+function equal4(){
+  var a= 10000  //set number value to a
+  var b=   100//set number value to b
+  return a/b;
+}
+function equal5(){
+  var a=  100//set number value to a
+  var b=   102//set number value to b
+  return a%b;
+}
